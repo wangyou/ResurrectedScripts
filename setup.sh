5a6d06f216138e8a969b7f9d8679d0b433bb30c6
Furrydaus <furrydaus@gmail.com>
2016-03-12 20:18:49 +0800

    Scrilog v2.1
    Revised Local Manifests from @darjwx to our own repository

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ sudo apt-get -y install bison build-essential curl flex git gnupg gperf \
 libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk2.8-dev libxml2 \
 libxml2-utils lzop maven openjdk-7-jdk openjdk-7-jre pngcrush schedtool \
 squashfs-tools xsltproc zip zlib1g-dev g++-multilib gcc-multilib \
-lib32ncurses5-dev lib32readline-gplv2-dev lib32z1-dev android-tools-adb
+lib32ncurses5-dev lib32readline-gplv2-dev lib32z1-dev android-tools-adb screen
 clear
 echo Dependencies have been installed!
 echo Settings up USB Ports...
